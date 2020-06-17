ef487b0de7e2234c152f2bd8ae3c4ab5c64d2c7b
ztt <7502762+zttf@user.noreply.gitee.com>
2020-06-17 10:17:41 +0800

    617

--- Java/practice/ClassDefAns/Company/Gnum/src/Test.java
+++ Java/practice/ClassDefAns/Company/Gnum/src/Test.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("请选择难度：");
-        Scanner sc =new Scanner(System.in);
-        int i=sc.nextInt();
+        //System.out.println("请选择难度：");
+        //Scanner sc =new Scanner(System.in);
+        //int i=sc.nextInt();
+        Gnum.choice();
     }
 }
